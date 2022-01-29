f61eed67d42299fdef181df26c6cfecd2883f597
thiagola92 <thiagola92@gmail.com>
2022-01-29 02:54:32 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,17 +6,20 @@ long_description = Path("README.md").read_text()
 setup(
     name="la-catch",
     version="0.0.1",
-    author="thiagola92",
-    author_email="thiagola92@gmail.com",
     description="Decorator to map exception to functions",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/thiagola92/la-catch",
+    author="thiagola92",
+    author_email="thiagola92@gmail.com",
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.10",
         "Operating System :: OS Independent",
     ],
-    packages=find_packages(exclude=["test*"]),
+    keywords="decorator, exception, catch",
+    license="MIT",
+    package_dir={'': 'la_catch'},
+    packages=find_packages(where="la_catch"),
     python_requires=">=3.10",
 )
